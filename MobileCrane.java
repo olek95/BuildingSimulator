c8b73a3d0638f1e8e86946ed8e4ddc321285d80f
AleksanderSklorz <olek1995s@poczta.onet.pl>
2017-02-14 14:27:09 +0100

    Update MobileCrane.java

--- MobileCrane.java
+++ MobileCrane.java
@@ -4,6 +4,13 @@ import com.jme3.scene.Spatial;
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.control.VehicleControl;
 import com.jme3.input.controls.ActionListener;
+import com.jme3.material.Material;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.texture.Texture;
+import java.util.List;
 public class MobileCrane implements ActionListener{
     private BuildingSimulator game = BuildingSimulator.getBuildingSimulator();
     private Spatial crane = game.getAssetManager().loadModel("Models/dzwig/dzwig.j3o");
@@ -15,6 +22,7 @@ public class MobileCrane implements ActionListener{
         control = crane.getControl(VehicleControl.class);
         crane.setLocalTranslation(0, 100, 0);
         game.getRootNode().attachChild(crane);
+        scaleTiresTexture();
         PhysicsSpace physics = game.getBulletAppState().getPhysicsSpace();
         physics.add(control);
     }
@@ -60,22 +68,34 @@ public class MobileCrane implements ActionListener{
                 control.steer(steeringValue);
         }
     }
-    public VehicleControl getControl(){
-        return control;
+    public void updateState(){
+        if(!forward && pressed && control.getCurrentVehicleSpeedKmHour() <= 0){
+            control.brake(0f);
+            control.accelerate(-brakeForce);
+        }else if(!pressed && control.getCurrentVehicleSpeedKmHour() < 1 && control.getCurrentVehicleSpeedKmHour() > -1)
+                stop();
     }
-    public boolean getForward(){
-        return forward;
+    private void scaleTiresTexture(){
+        List<Spatial> craneElements = ((Node)crane).getChildren();
+        Texture tireTexture = null;
+        for(Spatial element : craneElements)
+            if(element.getName().startsWith("wheel")){
+                Geometry tire = ((Geometry)((Node)((Node)element).getChild(0)).getChild(0));
+                Material tireMaterial = tire.getMaterial();
+                if(tireTexture == null){
+                    tireTexture = (Texture)tireMaterial.getTextureParam("DiffuseMap").getValue();
+                    tireTexture.setWrap(Texture.WrapMode.Repeat);
+                    tire.getMesh().scaleTextureCoordinates(new Vector2f(1,6f));
+                }else tireMaterial.setTexture("DiffuseMap", tireTexture);
+            }
     }
-    public boolean getPressed(){
-        return pressed;
-    }
-    public void setForward(boolean forward){
-        this.forward = forward;
-    }
-    public void setPressed(boolean pressed){
-        this.pressed = pressed;
-    }
-    public float getBrakeForce(){
-        return brakeForce;
+    private void stop(){
+        forward = true;
+        pressed = true;
+        control.accelerate(0f);
+        control.brake(0f);
+        control.setLinearVelocity(Vector3f.ZERO);
+        control.setAngularVelocity(Vector3f.ZERO);
     }
 }
+

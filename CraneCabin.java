a345d1621049b6fcdd360fe629591778791c2dfd
AleksanderSklorz <olek1995s@poczta.onet.pl>
2017-02-16 13:13:52 +0100

    Update CraneCabin.java

--- CraneCabin.java
+++ CraneCabin.java
@@ -3,18 +3,17 @@ package buildingsimulator;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.control.Control;
 
 public class CraneCabin implements AnalogListener{
-    private Spatial craneSpatial, craneCabin;
+    private Spatial craneSpatial, craneCabin, c;
     private float actualYRotation;
     public CraneCabin(Spatial craneSpatial){
         this.craneSpatial = craneSpatial;
-        craneCabin = ((Node)craneSpatial).getChild("craneCabin");
-        actualYRotation = 0;
+        craneCabin = ((Node)craneSpatial).getChild("crane");
     }
     public void onAnalog(String name, float value, float tpf) {
         actualYRotation += 0.1f;
+        System.out.println("b");
         switch(name){
             case "Right":
                 craneCabin.rotate(0, tpf, 0);

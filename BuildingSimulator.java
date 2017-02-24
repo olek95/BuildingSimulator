6b97f670bc1296f7ffaec44d0dc72535763b29af
AleksanderSklorz <olek1995s@poczta.onet.pl>
2017-02-24 19:27:08 +0100

    Update BuildingSimulator.java

--- BuildingSimulator.java
+++ BuildingSimulator.java
@@ -2,15 +2,20 @@ package buildingsimulator;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.joints.HingeJoint;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
 
 public class BuildingSimulator extends SimpleApplication implements ActionListener{
     private static BuildingSimulator game;
@@ -31,7 +36,7 @@ public class BuildingSimulator extends SimpleApplication implements ActionListen
         rootNode.attachChild(scene);
         stateManager.attach(bulletAppState);
         bulletAppState.getPhysicsSpace().add(rgb);
-        //bulletAppState.getPhysicsSpace().enableDebug(assetManager);
+        bulletAppState.getPhysicsSpace().enableDebug(assetManager);
         player = new MobileCrane();
         setupKeys(player);
         setupKeys(this);
@@ -39,11 +44,49 @@ public class BuildingSimulator extends SimpleApplication implements ActionListen
         sun.setColor(ColorRGBA.White);
         sun.setDirection(new Vector3f(-.5f,-.5f,-.5f).normalizeLocal());
         rootNode.addLight(sun);
+        
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        geom.setLocalTranslation(0, 0, 20);
+        Material mat = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+        RigidBodyControl rg = new RigidBodyControl(10f);
+        geom.addControl(rg);
+        bulletAppState.getPhysicsSpace().add(rg);
+        Box b2 = new Box(1, 1, 1); // create cube shape
+        Geometry geom2 = new Geometry("Box", b2);  // create cube geometry from the shape
+        geom2.setLocalTranslation(0, 1, 20);
+        Material mat2 = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat2.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom2.setMaterial(mat2);                   // set the cube's material
+        rootNode.attachChild(geom2);              // make the cube appear in the scene
+        RigidBodyControl rg2 = new RigidBodyControl(10f);
+        geom2.addControl(rg2);
+        bulletAppState.getPhysicsSpace().add(rg2);
+        /*Box b2 = new Box(1, 1, 1); // create cube shape
+        Geometry geom2 = new Geometry("Box", b2);  // create cube geometry from the shape
+        geom2.setLocalTranslation(0, 0, 10);
+        Material mat2 = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat2.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom2.setMaterial(mat2);                   // set the cube's material
+        rootNode.attachChild(geom2);              // make the cube appear in the scene
+        RigidBodyControl rg2 = new RigidBodyControl(10f);
+        geom2.addControl(rg2);
+        bulletAppState.getPhysicsSpace().add(rg2);
+        HingeJoint hj = new HingeJoint(rg, rg2, Vector3f.ZERO, new Vector3f(0f,-1,0f),
+                Vector3f.ZERO, Vector3f.ZERO);
+        hj.enableMotor(true, 0.1f, 0.1f);
+        bulletAppState.getPhysicsSpace().add(hj);*/
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //player.updateState();
+        player.updateState();
     }
 
     @Override

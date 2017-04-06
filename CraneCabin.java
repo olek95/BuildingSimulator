e7e1b2ce40dfda253c31f6b40b4a40e4f9435af7
AleksanderSklorz <olek1995s@poczta.onet.pl>
2017-04-06 20:24:00 +0200

    Update CraneCabin.java

--- CraneCabin.java
+++ CraneCabin.java
@@ -1,7 +1,6 @@
 package buildingsimulator;
 
 import com.jme3.bullet.PhysicsSpace;
-import com.jme3.bullet.collision.shapes.CompoundCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.control.VehicleControl;
 import com.jme3.bullet.joints.HingeJoint;
@@ -11,11 +10,6 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import static buildingsimulator.GameManager.*;
-import com.jme3.bounding.BoundingBox;
-import com.jme3.bullet.collision.PhysicsCollisionGroupListener;
-import com.jme3.bullet.collision.PhysicsCollisionObject;
-import com.jme3.collision.CollisionResults;
-import com.jme3.math.Ray;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,20 +20,18 @@ import java.util.List;
  * @author AleksanderSklorz
  */
 public class CraneCabin implements AnalogListener{
-    private Node craneCabin, lift, rectractableCranePart, mobileCrane, rope, ropeHook,
+    private Node craneCabin, lift, rectractableCranePart, mobileCrane,
             craneProps;
-    private Spatial hookHandle, hook, recentlyHitObject;
-    private float yCraneOffset = 0f, stretchingOut = 1f, hookLowering = 1f,
-            propsLowering = 1f, cranePropsProtrusion = 1f;
+    private float yCraneOffset = 0f, stretchingOut = 1f, propsLowering = 1f,
+            cranePropsProtrusion = 1f;
     private Vector3f hookHandleDisplacement, hookDisplacement, propDisplacement;
     private static final float MAX_PROTRUSION = 9.5f, MIN_PROTRUSION = 1f,
-            LIFTING_SPEED = 0.005f, STRETCHING_OUT_SPEED = 0.05f, 
-            HOOK_LOWERING_SPEED = 0.05f, PROP_LOWERING_SPEED = 0.05f,
+            LIFTING_SPEED = 0.005f, STRETCHING_OUT_SPEED = 0.05f, PROP_LOWERING_SPEED = 0.05f,
             CRANE_PROP_GOING_OUT_SPEED = 0.07f,
             MIN_CRANE_PROP_PROTRUSION = 1f;
     public static final float MAX_PROP_PROTRUSION = 6.35f, MIN_PROP_PROTRUSION = 1f;
-    private HingeJoint lineAndHookHandleJoint = null;
     private Geometry leftProtractilePropGeometry, rightProtractilePropGeometry;
+    private Hook hook;
     public CraneCabin(Spatial craneSpatial){
         mobileCrane = (Node)craneSpatial;
         craneCabin = (Node)mobileCrane.getChild("crane");
@@ -50,20 +42,12 @@ public class CraneCabin implements AnalogListener{
                 .getChild("leftProtractileProp")).getChild(0);
         rightProtractilePropGeometry = (Geometry)((Node)craneProps
                 .getChild("rightProtractileProp")).getChild(0);
-        hookHandle = lift.getChild("hookHandle");
         // do aktualnej kolizji dołącza kolizję z grupą 1
-        hookHandle.getControl(RigidBodyControl.class).addCollideWithGroup(1);
-        ropeHook = (Node)mobileCrane.getChild("ropeHook");
-        hook = ropeHook.getChild("hook");
-        rope = (Node)ropeHook.getChild("rope");
+        hook = new Hook((Node)mobileCrane.getChild("ropeHook"), lift.getChild("hookHandle"));
         createCranePhysics();
         hookHandleDisplacement = calculateDisplacementAfterScaling(rectractableCranePart, 
                 new Vector3f(1f, 1f, stretchingOut + STRETCHING_OUT_SPEED), false,
                 true, true);
-        hookDisplacement = calculateDisplacementAfterScaling(rope, 
-                new Vector3f(1f, hookLowering + HOOK_LOWERING_SPEED, 1f),
-                false, true, false);
-        hookDisplacement.y *= 2; // wyrównuje poruszanie się haka wraz z liną 
         propDisplacement =  calculateDisplacementAfterScaling((Node)mobileCrane
                 .getChild("protractileProp1"), new Vector3f(1f, propsLowering + PROP_LOWERING_SPEED,
                 1f), false, true, false);
@@ -97,23 +81,12 @@ public class CraneCabin implements AnalogListener{
                             stretchingOut -= STRETCHING_OUT_SPEED), false);
                 break;
             case "Lower hook":
-                CollisionResults results = new CollisionResults();
-                /* jeśli nie dotknęło żadnego obiektu, to zbędne jest sprawdzanie 
-                kolizji w dół*/
-                if(recentlyHitObject != null)
-                    new Ray(hook.getWorldTranslation(), new Vector3f(0,-0.5f,0))
-                            .collideWith((BoundingBox)recentlyHitObject.getWorldBound(),
-                            results); // tworzy pomocniczy promień sprawdzający kolizję w dół
-                // obniża hak, jeśli w żadnym punkcie z dołu nie dotyka jakiegoś obiektu
-                if(results.size() == 0) 
-                    changeHookPosition(rope, new Vector3f(1f, 
-                            hookLowering += HOOK_LOWERING_SPEED, 1f), false);
+                hook.lower();
                 break;
             case "Highten hook":
-                if(hookLowering > 1f) 
-                    changeHookPosition(rope, new Vector3f(1f, 
-                            hookLowering -= HOOK_LOWERING_SPEED, 1f), true);
-                recentlyHitObject = null;
+                if(hook.getHookLowering() > 1f) 
+                    hook.highten();
+                hook.setRecentlyHitObject(null);
         }
     }
     /**
@@ -155,50 +128,22 @@ public class CraneCabin implements AnalogListener{
         createObjectPhysics(rectractableCranePart, rectractableCranePart, 1f, true,
                 rectractableCranePart.getChild(0).getName());
         rectractableCranePart.getControl(RigidBodyControl.class).setCollisionGroup(3);
-        createRopeHookPhysics();
         HingeJoint cabinAndMobilecraneJoin = new HingeJoint(mobileCrane
                 .getControl(VehicleControl.class),(RigidBodyControl)craneCabin
                 .getControl(0), craneCabin.getLocalTranslation(), Vector3f.ZERO,
                 Vector3f.ZERO, Vector3f.ZERO);
         physics.add(cabinAndMobilecraneJoin);
         physics.add(lift.getChild("longCraneElement").getControl(0));
-        physics.add(hookHandle.getControl(0));
         /* Dodaje listener sprawdzający kolizję haka z obiektami otoczenia.
          Dla optymalizacji sprawdzam kolizję tylko dla grupy 2, czyli tej w 
          której znajduje sie hak.*/
-        physics.addCollisionGroupListener(new PhysicsCollisionGroupListener(){
-            public boolean collide(PhysicsCollisionObject nodeA, PhysicsCollisionObject nodeB){
-                if(nodeA.getUserObject().equals(ropeHook)
-                        && !nodeB.getUserObject().equals(hookHandle)){
-                    recentlyHitObject = (Spatial)nodeB.getUserObject();
-                }
-                return true;
-            }
-        }, 2);
-    }
-    private void changeHookPosition(Node scallingGeometryParent, Vector3f scallingVector,
-            boolean heightening){
-        movingDuringStretchingOut((Geometry)scallingGeometryParent.getChild(0), 
-                scallingVector, heightening, hook, hookDisplacement);
-        createRopeHookPhysics();
-    }
-    private void createRopeHookPhysics(){
-        Geometry ropeGeometry = (Geometry)rope.getChild(0);
-        CompoundCollisionShape ropeHookCompound = createCompound(rope, ropeGeometry.getName());
-        addNewCollisionShapeToComponent(ropeHookCompound, (Node)hook, "hookGeometry",
-                hook.getLocalTranslation(), hook.getLocalRotation());
-        createPhysics(ropeHookCompound, ropeHook, 4f, false, ropeGeometry);
-        RigidBodyControl ropeHookControl = ropeHook.getControl(RigidBodyControl.class);
-        ropeHookControl.setCollisionGroup(2); 
-        ropeHookControl.setCollideWithGroups(3);
-        lineAndHookHandleJoint = joinsElementToOtherElement(lineAndHookHandleJoint,
-                hookHandle, ropeHook, Vector3f.ZERO, new Vector3f(0, 0.06f,0));
+        physics.addCollisionGroupListener(hook.createCollisionListener(), 2);
     }
     private void changeHandleHookPoition(Node scallingGeometryParent, 
             Vector3f scallingVector, boolean pullingOut){
         Geometry rectractableCranePartGeometry = (Geometry)scallingGeometryParent.getChild(0);
         movingDuringStretchingOut(rectractableCranePartGeometry, scallingVector, 
-                pullingOut, hookHandle, hookHandleDisplacement);
+                pullingOut, hook.getHookHandle(), hookHandleDisplacement);
         createObjectPhysics(rectractableCranePart, rectractableCranePart, 1f, true,
                 rectractableCranePartGeometry.getName());
         rectractableCranePart.getControl(RigidBodyControl.class).setCollisionGroup(3);
@@ -220,3 +165,4 @@ public class CraneCabin implements AnalogListener{
         rightProtractilePropGeometry.setLocalScale(1f, cranePropsProtrusion, 1f);
     }
 }
+

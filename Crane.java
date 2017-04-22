0e3f169829d7b38d609c3178f6c9419c3b7a7465
AleksanderSklorz <olek1995s@poczta.onet.pl>
2017-04-22 14:48:15 +0200

    Update Crane.java
    dodanie komentarza, poprawne ustawienie fizyki dla dolnej części żurawia

--- Crane.java
+++ Crane.java
@@ -2,20 +2,30 @@ package buildingsimulator;
 
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 
+/**
+ * Obiekt klasy <code>Crane</code> reprezentuje żuraw. 
+ * @author AleksanderSklorz
+ */
 public class Crane {
     private BuildingSimulator game = BuildingSimulator.getBuildingSimulator();
     private Node crane = (Node)game.getAssetManager().loadModel("Models/zuraw/zuraw.j3o");
-    private Spatial prop;
     public Crane(){
         PhysicsSpace physics = game.getBulletAppState().getPhysicsSpace();
-        crane.setLocalTranslation(10f, -3f, 0f);
-        prop = crane.getChild("prop");
-        RigidBodyControl propControl = prop.getControl(RigidBodyControl.class);
-        propControl.setPhysicsLocation(prop.getLocalTranslation().add(10f, -3f, 0f));
-        physics.add(propControl);
+        Vector3f craneLocation = new Vector3f(10f, -1f, 0f);
+        crane.setLocalTranslation(craneLocation);
+        physics.add(setProperLocation(crane.getChild("prop"), craneLocation));
+        Spatial rack = crane.getChild("rack");
+        physics.add(setProperLocation(rack, craneLocation));
+        GameManager.setCrane(rack);
         game.getRootNode().attachChild(crane);
+    }
+    private RigidBodyControl setProperLocation(Spatial object, Vector3f displacement){
+        RigidBodyControl control = object.getControl(RigidBodyControl.class);
+        control.setPhysicsLocation(object.getLocalTranslation().add(displacement));
+        return control;
     }
 }

6b4eba41e37782d8f3ba207c5fccc9c7af4ba6cc
Aleksander Sklorz <aleksander.sklorz@smartstream-stp.com>
2017-08-30 12:25:51 +0200

    SelectBox dla kolorów zamiast RadioButton, ogólna metoda wypełniania listy jednowartościowymi elementami

--- src/menu/Options.java
+++ src/menu/Options.java
@@ -7,6 +7,7 @@ import com.jme3.system.AppSettings;
 import java.awt.DisplayMode;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.util.ArrayList;
 import tonegod.gui.controls.buttons.Button;
 import tonegod.gui.controls.buttons.CheckBox;
 import tonegod.gui.controls.buttons.RadioButton;
@@ -19,14 +20,13 @@ public class Options extends AbstractAppState  {
     private static Window optionsWindow;
     private static Screen screen = new Screen(BuildingSimulator.getBuildingSimulator());
     private static Options options;
-    private static RadioButtonGroup colorDepthButtonsGroup;
     private Options(){
         screen.parseLayout("Interface/options.gui.xml", this);
         optionsWindow = (Window)screen.getElementById("options");
         optionsWindow.getDragBar().setIsMovable(false);
         fillResolutionsSelectBox();
-        fillRefreshRateSelectBox();
-        addColorDepthButtonsToGroup(); 
+        fillSelectBoxSingleValue("refresh_rate_select_box");
+        fillSelectBoxSingleValue("color_depth_select_box");
     }
     
     public static void showOptions(){
@@ -47,59 +47,48 @@ public class Options extends AbstractAppState  {
         settings.setSamples(Integer.parseInt((String)((SelectBox)screen
                 .getElementById("antialiasing_select_box")).getSelectedListItem()
                 .getValue()));
+        settings.setFrequency((int)((SelectBox)screen
+                .getElementById("color_depth_select_box")).getSelectedListItem()
+                .getValue());
         settings.setFullscreen(((CheckBox)screen.getElementById("fullscreen_checkbox"))
                 .getIsChecked());
-        settings.setBitsPerPixel(Integer.parseInt(colorDepthButtonsGroup
-                .getSelected().getText().substring(0, 2)));
         BuildingSimulator game = BuildingSimulator.getBuildingSimulator(); 
         game.setSettings(settings);
         game.restart();
     }
     
     private void fillResolutionsSelectBox() {
-         GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment()
-                 .getDefaultScreenDevice();
-         DisplayMode[] modes = device.getDisplayModes();
+         DisplayMode[] modes = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice().getDisplayModes();;
+         ArrayList<String> elements = new ArrayList(); 
          SelectBox screenResolutions = (SelectBox)screen.getElementById("screen_resolution_select_box");
          for(int i = 0; i < modes.length; i++){
              String resolution = modes[i].getWidth() + "x" + modes[i].getHeight();
-             screenResolutions.addListItem(resolution, resolution);
+             if(!elements.contains(resolution))
+                screenResolutions.addListItem(resolution, resolution);
+             elements.add(resolution);
          }
     }
     
-    private void fillRefreshRateSelectBox() {
-        GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment()
-                .getDefaultScreenDevice();
-        DisplayMode[] modes = device.getDisplayModes();
-        SelectBox refreshRates = (SelectBox)screen.getElementById("refresh_rate_select_box");
+    private void fillSelectBoxSingleValue(String selectBoxId) {
+        DisplayMode[] modes = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice().getDisplayModes();
+        ArrayList<Integer> elements = new ArrayList(); 
+        SelectBox colorDepths = (SelectBox)screen.getElementById(selectBoxId);
+        String suffix;
+        int value; 
         for(int i = 0; i < modes.length; i++){
-            refreshRates.addListItem(modes[i].getRefreshRate() + "Hz", modes[i].getRefreshRate());
+            if(selectBoxId.equals("refresh_rate_select_box")){
+                suffix = "Hz";
+                value = modes[i].getRefreshRate();
+            }else{
+                suffix = " bpp";
+                value = modes[i].getBitDepth();
+            }
+            if(!elements.contains(value)){
+                colorDepths.addListItem(value + suffix, value);
+            }
+            elements.add(value);
         }
     }
-    
-    private void addColorDepthButtonsToGroup() {
-        colorDepthButtonsGroup = new RadioButtonGroup(screen,
-                "color_depth_group") {
-            @Override
-            public void onSelect(int index, Button value) {}
-        };
-        colorDepthButtonsGroup.addButton((RadioButton)screen
-                .getElementById("color_depth_16"));
-        colorDepthButtonsGroup.addButton((RadioButton)screen
-                .getElementById("color_depth_24"));
-        colorDepthButtonsGroup.addButton((RadioButton)screen
-                .getElementById("color_depth_32"));
-    }
-    
-//    private void fillAntialiasingSelectBox(){
-//        GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment()
-//                .getDefaultScreenDevice();
-//        GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().
-//        DisplayMode[] modes = device.getDisplayModes();
-//        SelectBox refreshRates = (SelectBox)screen.getElementById("antialiasing_select_box");
-//        refreshRates.addListItem("Disabled", 0);
-//        for(int i = 1; i < modes.length; i++){
-//            refreshRates.addListItem(modes[i].getRefreshRate() + "x", modes[i].getRefreshRate());
-//        }
-//    }
 }
